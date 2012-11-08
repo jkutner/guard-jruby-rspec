5f8ec60a6c9237ae216119e0542941a1bc79c82b
Doug Mayer <dougtmayer@gmail.com>
2012-11-08 14:42:55 -0600

    Guard::Rspec::Formatter is now a class, not a module.
    This corresponds to a guard-rspec change that made the formatter a class
    instead of a module:
      https://github.com/guard/guard-rspec/commit/31e1be02503c51ed37aeda486866550f7f8395d2

--- lib/guard/jruby-rspec/formatters/notification_rspec.rb
+++ lib/guard/jruby-rspec/formatters/notification_rspec.rb
@@ -1,8 +1,6 @@
 require "guard/rspec/formatter"
-require "rspec/core/formatters/base_formatter"
 
-class Guard::JRubyRSpec::Formatter::NotificationRSpec < RSpec::Core::Formatters::BaseFormatter
-  include Guard::RSpec::Formatter
+class Guard::JRubyRSpec::Formatter::NotificationRSpec < Guard::RSpec::Formatter
 
   def dump_summary(duration, total, failures, pending)
     message = guard_message(total, failures, pending, duration)
