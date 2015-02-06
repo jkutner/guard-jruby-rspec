b5b92cea05a3c362d458efb1f35a4e373f21bd06
Edward Anderson <nilbus@nilbus.com>
2015-02-05 21:41:21 -0500

    Support both guard-rspec 1.x and 2.x
    Related:
    https://github.com/guard/guard-rspec/commit/31e1be02503c51ed37aeda486866550f7f8395d2#diff-5

--- lib/guard/jruby-rspec/formatters/notification_rspec.rb
+++ lib/guard/jruby-rspec/formatters/notification_rspec.rb
@@ -1,6 +1,15 @@
 require "guard/rspec/formatter"
 
-class Guard::JRubyRSpec::Formatter::NotificationRSpec < Guard::RSpec::Formatter
+superclass = if Guard::RSpec::Formatter.instance_of? Module # guard-rspec-1.x
+               Object
+             elsif Guard::RSpec::Formatter.instance_of? Class # guard-rspec-2.x
+               Guard::RSpec::Formatter
+             else
+               fail 'Guard::RSpec::Formatter is neither class nor module'
+             end
+
+class Guard::JRubyRSpec::Formatter::NotificationRSpec < superclass
+  include Guard::RSpec::Formatter if Guard::RSpec::Formatter.instance_of? Module
 
   def dump_summary(duration, total, failures, pending)
     message = guard_message(total, failures, pending, duration)
